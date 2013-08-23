dde4facac5dfe4663197f88c0b3f4214d1000711
Tobias Schneider <schneider@blinkenlichts.net>
2013-08-23 22:32:04 +0200

    better formatting of the log messages

--- bin/lazzormanager.py
+++ bin/lazzormanager.py
@@ -3,7 +3,8 @@ import lazzormanagement
 import ConfigParser
 import logging
 import sys
-FORMAT = '%(asctime)-15s %(message)s'
+
+FORMAT = '%(asctime)-15s %(levelname)s: %(module)s:%(funcName)s(): %(message)s'
 logging.basicConfig(level = logging.INFO, format = FORMAT)
 
 manager = lazzormanagement.LazzorManager(sys.argv[1])
